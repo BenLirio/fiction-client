aa6ed9481bcb61dcbfb41f11b7f62986183d7642
BenLirio <benjlirio@gmail.com>
2020-01-27 12:37:24 -0500

    Add basic sign in modal

---
+++ src/Modal/SignIn/SignIn.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { Modal, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  }
+}))
+
+const SignIn = () => {
+  const classes = useStyles()
+  const [open] = useState(false)
+  return (
+    <div>
+      <Modal open={open}>
+        <div className={classes.paper}>
+          <h2 id="simple-modal-title">Text in a modal</h2>
+          <p id="simple-modal-description">
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </p>
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default SignIn
