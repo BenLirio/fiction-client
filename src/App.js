a5992ed662a7114563c54f100ea30d4a264e9184
BenLirio <benjlirio@gmail.com>
2020-01-27 13:26:06 -0500

    Add auto sign in for dev purposes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import Layout from './Layout/Layout'
 import Main from './views/Main'
+import { signIn } from './hooks/auth'
+import userContext from './user-context'
 
 const App = () => {
+  const { setToken } = useContext(userContext)
+  useEffect(() => {
+    signIn({ email: 'test@gmail.com', password: 'test' })
+      .then(({ data: { user: { token } } }) => setToken(token))
+      .then(() => {
+        console.log('============= signed in =============')
+      })
+  }, [])
   return (
     <Layout>
       <Main />
