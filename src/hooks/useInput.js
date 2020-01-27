005c4acfb0b86392f91c02d33b8a727f9539b72f
BenLirio <benjlirio@gmail.com>
2020-01-27 12:55:12 -0500

    Add use input

---
+++ src/hooks/useInput.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+export const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue)
+  const bind = {
+    value,
+    onChange: e => setValue(e.target.value)
+  }
+  const reset = () => setValue('')
+  return [value, bind, reset]
+}
+export default useInput
