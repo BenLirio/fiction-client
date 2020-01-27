23023763ae7428138b49a22fa4ad00816814bc66
BenLirio <benjlirio@gmail.com>
2020-01-27 10:07:13 -0500

    deploy task

---
+++ precache-manifest.d2d08229b6b4d906b7a7d9e6a76619d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415f7637c251937dcd72e64e3f38542d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa33b1c6c9a9d7216f9e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "843394c4e87b37cd99b2",
+    "url": "/static/js/2.17d89735.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.17d89735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fa33b1c6c9a9d7216f9e",
+    "url": "/static/js/main.c54a5452.chunk.js"
+  },
+  {
+    "revision": "1289ac55f7a00a80112c",
+    "url": "/static/js/runtime-main.b937fd36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
